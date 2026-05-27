3271792ed8c79195dd8d6cbfdec0e4a272a202dd
krystiankapusta <krystian.kapusta@student.up.krakow.pl>
2026-05-27 22:26:54 +0200

    initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.krystiankapusta"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
